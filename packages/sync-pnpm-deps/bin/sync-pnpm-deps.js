85e11d6404deaf28c4bb05f228cda84466fc4557
zanminkian <hellozmj@qq.com>
2023-04-10 11:27:23 +0000

    refactor: optimize bin

--- packages/sync-pnpm-deps/bin/sync-pnpm-deps.js
+++ packages/sync-pnpm-deps/bin/sync-pnpm-deps.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
-import { Command } from 'commander'
+import { program } from 'commander'
 import { check } from '../dist/cli.js'
 
-const program = new Command().name('sync-pnpm-deps')
-
 program
+  .name('sync-pnpm-deps')
   .command('check')
   .description('check monorepo\'s pnpm-lock.yaml file to ensure deps are the same in one app')
   .option('-d, --dir <path>', 'dir path to load `pnpm-lock.yaml`', '.')
   .option('-p, --prod', 'check production dependencies only and skip to check devDependencies')
   .action(check)
-
-program.parse()
+  .parse()
